1ec7c1a734a4cf0b4b550dc9e03236145d67d783
Brayden Howells <braydenhowells2003@gmail.com>
2025-03-13 00:41:56 -0600

    some game service tests now working

--- server/src/test/java/service/GameServiceTest.java
+++ server/src/test/java/service/GameServiceTest.java
@@ -1,94 +1,103 @@
-//package service;
-//
-//import chess.ChessGame;
-//import dataaccess.*;
-//import handlers.MasterHandler;
-//import model.GameData;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//
-//
-//import java.sql.SQLException;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//class GameServiceTest {
-//    static UserDAO userDAO = new MemoryUserDao();
-//    static AuthDAO authDAO = new MemoryAuthDao();
-//    static GameDAO gameDAO = new MemoryGameDao();
-//    static AuthService authService = new AuthService(authDAO);
-//    static UserService userService = new UserService(userDAO, authDAO, authService);
-//    static GameService gameService = new GameService(gameDAO, authService);
-//    static MasterHandler handler = new MasterHandler(userService, gameService, authService);
-//
-//    @BeforeEach
-//    void reset() throws SQLException {
-//        gameDAO.clear();
-//        userDAO.clear();
-//        authDAO.clear();
-//    }
-//
-////    @Test
-////    void clear() throws SQLException {
-////        gameDAO.create(new GameData(1, "white", "black", "ggNoRe", new ChessGame()));
-////        gameDAO.clear();
-////
-////        assertEquals(0, gameDAO.findAll().size());
-////    }
-//
-////    @Test
-////    void create() {
-////        assertEquals(1, gameDAO.findAll().size());
-////    }
-//
-////    @Test
-////    void getGames() {
-////        ListResult result = gameService.getGames("gang", new Response());
-////
-////        assertEquals(2, result.games().size());
-////    }
-//
-////    @Test
-////    void findGame() {
-////        GameData data = new GameData(1, "white", "black", "newGame", new ChessGame());
-////        gameDAO.create(data);
-////        GameData dataCheck = gameService.findGame("1");
-////
-////        assertEquals(data, dataCheck);
-////    }
-//
-////    @Test
-////    void updateGameUser() {
-////        GameData data = new GameData(1, "", "", "MrGame_N_Watch", new ChessGame());
-////        gameDAO.create(data);
-////        gameService.join("white", data, "WHITE");
-////
-////        GameData newData = gameDAO.find("1");
-////        assertEquals("white", newData.whiteUsername());
-////    }
-//
-//
-//    @Test
-//    void createFail() {
-//        // the logic for this method is in the handler, and it does not really check anything
-//        // this means that errors are not returned and I cannot replicate these errors
-//    }
-//
-//    @Test
-//    void getGamesFail() {
-//        // the logic for this method is in the handler, and it does not really check anything
-//        // this means that errors are not returned and I cannot replicate these errors
-//    }
-//
-//    @Test
-//    void findGameFail() {
-//        // the logic for this method is in the handler, and it does not really check anything
-//        // this means that errors are not returned and I cannot replicate these errors
-//    }
-//
-//    @Test
-//    void updateGameUserFail() {
-//        // the logic for this method is in the handler, and it does not really check anything
-//        // this means that errors are not returned and I cannot replicate these errors
-//    }
-//}
+package service;
+
+import chess.ChessGame;
+import dataaccess.*;
+import handlers.MasterHandler;
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+import requests.JoinRequest;
+import results.ListResult;
+import results.LoginResult;
+import requests.RegisterRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.sql.SQLException;
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameServiceTest {
+    static UserDAO userDAO = new SQLUserDao();
+    static AuthDAO authDAO = new SQLAuthDao();
+    static GameDAO gameDAO = new SQLGameDao();
+    static AuthService authService = new AuthService(authDAO);
+    static UserService userService = new UserService(userDAO, authDAO, authService);
+    static GameService gameService = new GameService(gameDAO, authService);
+    static MasterHandler handler = new MasterHandler(userService, gameService, authService);
+
+    @BeforeEach
+    void reset() throws SQLException {
+        gameDAO.clear();
+        userDAO.clear();
+        authDAO.clear();
+    }
+
+    @Test
+    void clear() throws SQLException {
+        gameDAO.create(new GameData(1, "white", "black", "ggNoRe", new ChessGame()));
+        gameDAO.clear();
+        assertEquals(0, gameDAO.findAll().size());
+    }
+
+    @Test
+    void create() throws SQLException {
+        assertEquals(0, gameDAO.findAll().size());
+        gameDAO.create(new GameData(1, "white", "black", "newGame", new ChessGame()));
+        assertEquals(1, gameDAO.findAll().size());
+    }
+
+    @Test
+    void getGames() throws SQLException {
+        gameDAO.create(new GameData(1, "white", "black", "Game1", new ChessGame()));
+        gameDAO.create(new GameData(2, "white2", "black2", "Game2", new ChessGame()));
+        ListResult result = gameService.getGames("authToken");
+        assertNotNull(result);
+        assertEquals(2, result.games().size());
+    }
+
+    @Test
+    void findGame() throws SQLException {
+        GameData data = new GameData(1, "white", "black", "newGame", new ChessGame());
+        gameDAO.create(data);
+        GameData dataCheck = gameService.findGame("1");
+        assertNotNull(dataCheck);
+        assertEquals(data, dataCheck);
+    }
+
+    @Test
+    void updateGameUser() throws SQLException {
+        GameData gameData = new GameData(1, null, null, "MrGame_N_Watch", new ChessGame());
+        AuthData authData = new AuthData("epicToken", "CaptainFalcon");
+        gameDAO.create(gameData);
+        authDAO.createAuth(authData);
+        gameService.join(gameData, "WHITE", "epicToken", new JoinRequest("WHITE", "1"));
+        GameData newData = gameDAO.find("1");
+        assertEquals("CaptainFalcon", newData.whiteUsername());
+    }
+
+    @Test
+    void createFail() {
+        assertThrows(SQLException.class, () -> gameDAO.create(null));
+    }
+
+    @Test
+    void getGamesFail() {
+        assertThrows(SQLException.class, () -> gameService.getGames(null));
+    }
+
+    @Test
+    void findGameFail() {
+        assertNull(gameService.findGame("99999"));
+    }
+
+    @Test
+    void updateGameUserFail() throws SQLException {
+        GameData gameData = new GameData(1, null, null, "MrGame_N_Watch", new ChessGame());
+        UserData userData = new UserData("CaptainFalcon", "yuh", "yuh");
+        AuthData authData = new AuthData("sadToken", "CaptainFalcon");
+        gameDAO.create(gameData);
+        authDAO.createAuth(authData);
+        gameService.join(gameData, "WHITE", "epicToken", new JoinRequest("WHITE", "1"));
+        GameData newData = gameDAO.find("1");
+        assertEquals("CaptainFalcon", newData.whiteUsername());
+    }
+}
