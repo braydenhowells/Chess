c5ed99c345a623d64e00975480d2c051ef0b98dc
Brayden Howells <braydenhowells2003@gmail.com>
2025-03-19 23:18:47 -0600

    moved server facade and also added response exceptions for errors

---
+++ server/src/main/java/server/ServerFacade.java
@@ -0,0 +1,102 @@
+package server;
+
+import com.google.gson.Gson;
+
+import java.io.*;
+
+
+import exception.ResponseException;
+import requests.JoinRequest;
+import requests.RegisterRequest;
+import results.SimpleResult;
+
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+
+public class ServerFacade {
+
+    private final String serverUrl;
+
+    public ServerFacade(String serverUrl) {
+        this.serverUrl = serverUrl;
+    }
+
+    public SimpleResult register(RegisterRequest request) {
+        // fill with actual stuff
+
+        // var path = "/register";
+        // return this.makeRequest(filled with params)
+        return new SimpleResult(null);
+    }
+
+    public SimpleResult join(JoinRequest request) {
+        // and so forth
+        return new SimpleResult(null);
+    }
+
+    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) throws ResponseException {
+        try {
+            URL url = (new URI(serverUrl + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
+
+            writeBody(request, http);
+            http.connect();
+            throwIfNotSuccessful(http);
+            return readBody(http, responseClass);
+        }
+        // pet shop has this double catch here. might as well include it eh?
+        catch (ResponseException e) {
+            throw e;
+        }
+        catch (Exception e) {
+            System.out.println("uh oh, an error occurred in the makeRequest method");
+            throw new ResponseException(500, e.getMessage());
+        }
+    }
+
+
+    private static void writeBody(Object request, HttpURLConnection http) throws IOException {
+        if (request != null) {
+            http.addRequestProperty("Content-Type", "application/json");
+            String reqData = new Gson().toJson(request);
+            try (OutputStream reqBody = http.getOutputStream()) {
+                reqBody.write(reqData.getBytes());
+            }
+        }
+    }
+
+    private void throwIfNotSuccessful(HttpURLConnection http) throws IOException, ResponseException {
+        var status = http.getResponseCode();
+        if (!isSuccessful(status)) {
+            try (InputStream respErr = http.getErrorStream()) {
+                if (respErr != null) {
+                    throw ResponseException.fromJson(respErr);
+                }
+            }
+
+            throw new ResponseException(status, "other failure: " + status);
+        }
+    }
+
+    private static <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
+        T response = null;
+        if (http.getContentLength() < 0) {
+            try (InputStream respBody = http.getInputStream()) {
+                InputStreamReader reader = new InputStreamReader(respBody);
+                if (responseClass != null) {
+                    response = new Gson().fromJson(reader, responseClass);
+                }
+            }
+        }
+        return response;
+    }
+
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
+    }
+
+}
