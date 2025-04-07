a1211a7459aa22d7b6264bbdba31ccb8c01533b3
Brayden Howells <braydenhowells2003@gmail.com>
2025-04-06 23:07:50 -0600

    mailman can broadcast now!! heck yeah. passed test 2

--- server/src/main/java/server/WSServerMailman.java
+++ server/src/main/java/server/WSServerMailman.java
@@ -10,13 +10,21 @@ import websocket.commands.UserGameCommand;
 import websocket.messages.ServerMessage;
 
 import java.io.IOException;
+import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class WSServerMailman {
 
     private final AuthService authService;
     private final GameService gameService;
+    private final ConcurrentHashMap<String, Session> activeSessions = new ConcurrentHashMap<>();
+    // keeps ^^ track of ALL the sessions in our app. key = username, val = session
+    private final ConcurrentHashMap<Integer, CopyOnWriteArrayList<String>> gameMembers = new ConcurrentHashMap<>();
+    // keeps ^^ track of ALL the games currently attended by players / observers. key = gameID, val = username set
+
+
 
     // track all active sessions
     private final Set<Session> sessions = ConcurrentHashMap.newKeySet();
@@ -29,6 +37,7 @@ public class WSServerMailman {
     public void onConnect(Session session) {
         sessions.add(session);
         System.out.println("a client connected");
+
     }
 
     public void onDisconnect(Session session) {
@@ -66,7 +75,20 @@ public class WSServerMailman {
                 return;
             }
 
-            String username = authData.username();
+            // add the session to our map after we know that sucker is legit
+            String currentUsername = authData.username();
+            activeSessions.put(currentUsername, session);
+
+            // add user to the list of game members
+            CopyOnWriteArrayList<String> members = gameMembers.get(gameID);
+            if (members == null) {
+                members = new CopyOnWriteArrayList<>();
+                gameMembers.put(gameID, members);
+            }
+            if (!members.contains(currentUsername)) { // make sure that we do not already have this user
+                members.add(currentUsername); // not having this check makes some tests fail
+            }
+
 
             // get game
             GameData gameData = gameService.findGame(String.valueOf(gameID));
@@ -75,29 +97,51 @@ public class WSServerMailman {
                 return;
             }
 
-            // check if player or observer
-            boolean isPlayer = username.equals(gameData.whiteUsername()) || username.equals(gameData.blackUsername());
-            boolean isObserver = !isPlayer;
+            // send LOAD_GAME to the root client
+            ServerMessage loadGame = new ServerMessage(ServerMessage.ServerMessageType.LOAD_GAME);
+            loadGame.setGame(gameData);
+            System.out.println("sending LOAD_GAME to client:"); // debug stuff
+            System.out.println(new Gson().toJson(loadGame)); // debug stuff
+            sendMessage(session, loadGame);
 
-            if (!isPlayer && !isObserver) {
-                sendError(session, "you are not part of this game");
-                return;
+            // build NOTIFICATION for other members in game (if they exist)
+            ServerMessage joinNoti = new ServerMessage(ServerMessage.ServerMessageType.NOTIFICATION);
+
+            if (currentUsername.equals(gameData.whiteUsername())) {
+                joinNoti.setMessage(currentUsername + " joined as WHITE");
+            } else if (currentUsername.equals(gameData.blackUsername())) {
+                joinNoti.setMessage(currentUsername + " joined as BLACK");
+            } else {
+                joinNoti.setMessage(currentUsername + " joined as OBSERVER");
             }
 
-            // send game back
-            ServerMessage response = new ServerMessage(ServerMessage.ServerMessageType.LOAD_GAME);
-            response.setGame(gameData);
-            // debug
-            System.out.println("sending LOAD_GAME to client:");
-            System.out.println(new Gson().toJson(response));
-
-            sendMessage(session, response);
+            // broadcast NOTIFICATION yessah
+            System.out.println("broadcasting notification:"); // debug stuff
+            System.out.println(new Gson().toJson(joinNoti)); // debug stuff
+            broadcastMessage(gameID, joinNoti, currentUsername);
 
         } catch (Exception e) {
             e.printStackTrace();
             sendError(session, "internal server error");
         }
     }
+
+    // method for sending messages to all users in a game except 1
+    private void broadcastMessage(int gameID, ServerMessage message, String excludeThisUser) {
+        CopyOnWriteArrayList<String> members = gameMembers.get(gameID);
+        if (members == null) return;
+
+        for (String username : members) {
+            if (Objects.equals(username, excludeThisUser)) {
+                continue;
+            }
+            Session session = activeSessions.get(username);
+            if (session != null && session.isOpen()) {
+                sendMessage(session, message);
+            }
+        }
+    }
+
 
     private void sendMessage(Session session, ServerMessage msg) {
         try {
